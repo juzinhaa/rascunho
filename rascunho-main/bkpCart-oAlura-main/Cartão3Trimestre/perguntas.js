bdea1314ea056b6f624526577f749f5e439b4131
juzinhaa <142804545+juzinhaa@users.noreply.github.com>
2024-10-29 09:57:23 -0300

    Add files via upload

---
+++ rascunho-main/bkpCart-oAlura-main/Cartão3Trimestre/perguntas.js
@@ -0,0 +1,26 @@
+criaCartao(
+    //categoria,
+    'Programação',
+    //pergunta,
+    'Oque é JavaScript',
+    //resposta,
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    //categoria,
+    'Programação',
+    //pergunta,
+    'Oque é JavaScript',
+    //resposta,
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    //categoria,
+    'Programação',
+    //pergunta,
+    'Oque é JavaScript',
+    //resposta,
+    'O JavaScript é uma linguagem de programação'
+)
