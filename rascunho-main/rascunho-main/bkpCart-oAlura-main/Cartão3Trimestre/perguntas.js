2b222bc2ed0ddf981713838d6fb642ed03cee5b4
juzinhaa <142804545+juzinhaa@users.noreply.github.com>
2024-11-05 10:51:47 -0300

    Add files via upload

---
+++ rascunho-main/rascunho-main/bkpCart-oAlura-main/Cartão3Trimestre/perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    //categoria,
+    'Programação',
+    //pergunta,
+    'Oque é JavaScript',
+    //resposta,
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+  
+    'Programação',
+    'Qual a capital da frança',
+    'A capital da Framça é Paris'
+)
+
+criaCartao(
+    
+    'Programação',
+    'Oque é uma função',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+   
+    'Literatura',
+    'Qual é o nome da Saga de Crepusculo',
+    'O nome é Crepusculo'
+)
